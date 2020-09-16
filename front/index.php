367ecee3cb31c742361a83197f880368fd4f4393
Kona <kitami.kitano20@gmail.com>
2020-09-16 20:40:39 +0900

    [add]HTML,CSSを作成 雛形を作成しました。

--- front/index.php
+++ front/index.php
@@ -1,18 +1,85 @@
-<?php
-echo '<h1>トップページ</h1>';
-?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+    integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
   <link rel="stylesheet" href="../style/style.css">
   <title>トップページ</title>
 </head>
 
 <body>
+  <!-- Header -->
+  <header>
+    <div class="container pt-5 pb-5">
+      <div class="row">
+        <div class="col">
+          <h2><a href="#" class="top">OSIshareApp</a></h2>
+        </div>
+        <div class="col-md-auto">
+          <a href="#">SignUp</a>
+        </div>
+        <div class="col-md-auto">
+          <a href="#">SignIn</a>
+        </div>
+      </div>
+    </div>
+  </header>
 
+  <!-- Main -->
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="card" style="width: 100%;">
+            <img src="../images/image2.jpg" alt="" class="bd-placeholder-img card-img-top" width="100%" height="200">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card" style="width: 100%;">
+            <img src="../images/image3.jpg" alt="" class="bd-placeholder-img card-img-top" width="100%" height="200">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card" style="width: 100%;">
+            <img src="../images/image1.jpg" alt="" class="bd-placeholder-img card-img-top" width="100%" height="200">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </main>
+
+
+  <script src=" https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+  </script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+    integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+  </script>
 </body>
 
 </html>
